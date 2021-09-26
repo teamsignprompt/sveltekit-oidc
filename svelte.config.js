bf8a4f66b24d17c305bf038b18044d594e791466
Kevin Wright <kevin@signprompt.com>
2021-09-26 12:32:18 -0400

    Testing vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,6 +14,7 @@ const config = {
 		package: {
 			dir: 'sveltekit-oidc',
 		},
+		adapter: vercel()
 	}
 };
 
