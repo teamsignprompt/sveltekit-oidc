5fc3a13086be16a81aabf1df674109649b0cf197
Kevin Wright <kevin@signprompt.com>
2021-09-27 13:40:28 -0400

    Header in todo index

--- src/routes/todos/index.svelte
+++ src/routes/todos/index.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import { KeycloakProtectedRoute, LogoutButton } from '$lib';
+    import Header from '../../components/shared/Header/index.svelte';
 </script>
 
 <KeycloakProtectedRoute>
+    <Header></Header>
     <div class="h-screen-minus-navbar bg-gray-800 text-white flex flex-col justify-center items-center w-full">
         This is a protected page
         <LogoutButton>Logout</LogoutButton>
